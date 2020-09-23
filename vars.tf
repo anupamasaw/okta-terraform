249250c0959bc1201ff7dc9414825fb1c345597c
anupamasaw <anupamasaw80@gmail.com>
2020-09-23 17:27:03 +0530

    new commit

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "okta_group_name" {
+  default = "test11"
+}
+variable "okta_group_description" {
+  default = "my xyz group"
+}
+
+variable "okta_api_token" {
+  default = "00Mgy41rj_z6IBgrJFVyb9lYmAzXoDPkhYNTJHatOV"
+}
