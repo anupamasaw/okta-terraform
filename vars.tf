433da6fcf3a19243ee3b32dfc1fa8002529d48d0
anupamasaw <56156879+anupamasaw@users.noreply.github.com>
2020-09-30 11:09:50 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,6 @@
 variable "okta_group_name" {}
 
-variable "okta_group_description" {
-  default = "my xyz group"
-}
+variable "okta_group_description" {}
 
 variable "okta_api_token" {
   default = "00kH77HMsG1jt3U4ownF4d-_pjMIS9gmonqQCPYywM"
