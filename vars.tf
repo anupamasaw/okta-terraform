00a15315dfbc92398570f91dfe5cde8e0849e5cb
anupamasaw <56156879+anupamasaw@users.noreply.github.com>
2020-10-27 12:14:43 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,5 +5,5 @@ variable "okta_group_description" {
 }
 
 variable "okta_api_token" {
-  default = "00kH77HMsG1jt3U4ownF4d-_pjMIS9gmonqQCPYywM"
+  default = ""00Ohn6fBNfzMbGGuMsN8ajXHIuO1zBZnmf67IFMI3"
 }
